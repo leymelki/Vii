124c01c900711b6b3c082a774bf1ac758c07e0a4
Semen Alekseev <semyon492@ya.ru>
2021-02-14 10:35:21 +0300

    Update mysql.php

--- system/classes/mysql.php
+++ system/classes/mysql.php
@@ -1,15 +1,5 @@
 <?php
-/*
-	Appointment: Класс для работы с базой данных
-	File: mysql.php
-	Author: f0rt1 
-	Engine: Vii Engine
-	Copyright: NiceWeb Group (с) 2011
-	e-mail: niceweb@i.ua
-	URL: http://www.niceweb.in.ua/
-	ICQ: 427-825-959
-	Данный код защищен авторскими правами
-*/
+
 if (!defined('MOZG')) die('Hacking attempt!');
 class db {
     var $db_id = false;
